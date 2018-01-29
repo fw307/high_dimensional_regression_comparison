a48004203fb78b7080bf66b8d5f90ef2a74e628e
Fan Wang <31932803+fw307@users.noreply.github.com>
2018-01-29 13:46:41 +0000

    Update figure6.R

--- plotting_code_main_simulation/figure6.R
+++ plotting_code_main_simulation/figure6.R
@@ -164,7 +164,5 @@ mtext("SNR=1",at=0.77,side=3,outer=T,cex=1,font=2)
 
 
 all_methods_text=paste(method_seq,collapse=",") 
-main=sprintf("Pairwise correlation,n=%s,p=%s,s0=%s,SNR=%s",n,p,s0,SNR) 
-MAIN=main
 par(fig=c(0,1,0,1),oma=c(0,0,0,0),mar=c(0,0,0,0),new=TRUE); plot(0,0,type="n",bty="n",xaxt="n",yaxt="n")
 legend("bottom",c("Lasso","HENet","Ridge","Dantzig","SCAD","Stability"),xpd=TRUE,horiz=TRUE,inset=c(0,0),lty=rep(1,4),bty="n",col=colors.all,cex=1,lwd=3)
